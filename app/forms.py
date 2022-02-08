333d70c69d46c261502e80db5397c450b7a4105f
OCowper <ZKAC355@live.rhul.ac.uk>
2022-02-08 20:34:27 +0000

    added payment screen

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
+
 class enterCustID(FlaskForm):
     customerID = StringField('Customer Number')
     submit = SubmitField('Next')
 
 class enterTime(FlaskForm):
     timePeriod = StringField('Hour Number')
-    submit = SubmitField('Enter Car Park')
+    submit = SubmitField('Next')
     
-    
+class paymentForm(FlaskForm):
+    submit = SubmitField('Done: Enter Car Park')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
-from app.forms import enterCustID
+from app.forms import *
 
 @app.route("/")
 @app.route("/index")
@@ -18,5 +18,13 @@ def login():
 def time():
     form = enterTime()
     if form.validate_on_submit():
-        return redirect('/index')
+        return redirect('/payment')
     return render_template('enterTime.html', title = 'How long are you staying?', form=form)
+
+@app.route('/payment', methods = ['GET', 'POST'])
+def payment():
+    form = paymentForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('enterPayment.html', title = 'Please Pay Now', form = form)
+
