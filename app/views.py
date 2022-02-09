ddc982cf50780142d09975d3c40ce88961359934
zkac421 <zkac421@live.rhul.ac.uk>
2022-02-09 00:28:37 +0000

    Added functionality to calculate the parking price (demo) based on customerID and timePeriod

--- app/views.py
+++ app/views.py
@@ -1,6 +1,37 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import *
+
+
+class DataHandler:
+    def __init__(self):
+        self.customerID = None # str
+        self.timePeriod = None # int
+        self.discount = None # int
+
+    def getDiscount(self):
+        if self.customerID.endswith("2"):
+            return 0.5
+
+        elif self.customerID.endswith("5"):
+            return 0
+
+        else:
+            return 1
+
+    def calculatePrice(self):
+        price = 0
+        if self.timePeriod < 7:
+            price = self.timePeriod * 2 # £2
+
+        else:
+            price = 20
+
+        return price - (price * self.getDiscount())
+
+
+data = DataHandler()
+
 
 @app.route("/")
 @app.route("/index")
@@ -9,6 +40,10 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        formData = dict(request.form)
+        data.customerID = formData["customerID"]
+
     form = enterCustID()
     if form.validate_on_submit():
             return redirect('/time')
@@ -16,15 +51,20 @@ def login():
 
 @app.route('/time', methods = ['GET', 'POST'])
 def time():
+    if request.method == "POST":
+        formData = dict(request.form)
+        data.timePeriod = int(formData["timePeriod"])
+
     form = enterTime()
     if form.validate_on_submit():
         return redirect('/payment')
     return render_template('enterTime.html', title = 'How long are you staying?', form=form)
 
+    
 @app.route('/payment', methods = ['GET', 'POST'])
 def payment():
     form = paymentForm()
     if form.validate_on_submit():
         return redirect('/index')
-    return render_template('enterPayment.html', title = 'Please Pay Now', form = form)
 
+    return render_template('enterPayment.html', title = 'Please Pay Now', form = form, price=f"{data.calculatePrice()}:02")
